20daf69a55995971c7e6a34879d962d840bab4a9
dontforgetsemicologne <semicologne13@outlook.com>
2026-01-23 22:41:04 +0530

    feat: add SVG components for Backward, Forward, and Flag icons
    - Introduce new SVG components: Backward, Forward, and Flag, enhancing the visual representation in the application.
    - Each component includes appropriate SVG markup and titles for accessibility.

---
+++ src/components/svg/backward.tsx
@@ -0,0 +1,18 @@
+export default function Backward() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 24 24"
+      role="presentation"
+    >
+      <title>Backwards</title>
+      <path
+        fill="currentColor"
+        d="m14 18-6-6 6-6 1.4 1.4-4.6 4.6 4.6 4.6z"
+      ></path>
+    </svg>
+  );
+}
